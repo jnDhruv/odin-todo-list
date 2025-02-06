18216cee8c3355b1643a9f69d80e688717ce52aa
Dhruv <dhruvjain.dev@gmail.com>
2025-02-06 09:14:10 +0530

    Add close buttons + fix button styling in dialogs

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import loadProjectPage from "./modules/ui/loadProjectPage";
 
 import './styles/layout.css';
 
-sidebar.render();
-loadProjectPage.init();
+function assignEventListeners() {
+    const closeDiagButton = document.querySelectorAll(".close-dialog-button");
+    closeDiagButton.forEach(button => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            button.parentElement.parentElement.reset();
+            button.parentElement.parentElement.parentElement.close();
+        });
+    })
+}
+
+function init() {
+    assignEventListeners();
+    sidebar.render();
+    loadProjectPage.init();
+}
+
+init();
